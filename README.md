0aa93aec86b909966c74b4548b313a0b343856d3
Jacques Supcik <jacques.supcik@hefr.ch>
2019-09-05 08:27:43 +0200

    feat: add options

--- README.md
+++ README.md
@@ -18,16 +18,43 @@ and only publishing them at a given date.
      - select-files:
       select: '^s(\d+)'
       where: 'lambda x : int(x) <= 5'
-      
    ```
 
   This would search for files named `sNN...` and select only those where `NN` is
   less than or equal to `5`.
 
+  In the `where` expression, you can use the following declared variables
+  - `now` : represent the current time in [ISO format](https://docs.python.org/3/library/datetime.html#datetime.datetime.isoformat)
+  - `sfc` : The value of the `SELECT_FILE_CONDITION` environment variable.
+
+  The modules `os` and `datetime` are imported, so you can use methods from them.
+
+## Disabling the plugin
+
+You can disable the plugin using an environment variable. For example, you could configure
+your site this way:
+
+```yaml
+plugins:
+  - select-files:
+  select: '^s(\d+)'
+  where: 'lambda x : int(x) <= 5'
+  disabled_if_env: SELECT_FILES_DISABLED
+```
+
+and then, running mkdocs with `SELECT_FILES_DISABLED` set to 1 would disable this plugin
+and let all pages be processed:
+
+``` bash
+SELECT_FILES_DISABLED=1 mkdocs ...
+```
+
 ## Credits
 
-Thank you to [Lucy Linder](https://github.com/derlin) for her great idea of using parametrized regular expressions.
+Thank you to [Lucy Linder](https://github.com/derlin) for her great idea of using parametrized
+regular expressions.
 
 ## Similar plugins
 
-- [mkdocs-exclude](https://github.com/apenwarr/mkdocs-exclude) : exclude arbitrary file paths and patterns from the input
+- [mkdocs-exclude](https://github.com/apenwarr/mkdocs-exclude) : exclude arbitrary file paths and
+patterns from the input
--- selectfiles/plugin.py
+++ selectfiles/plugin.py
@@ -1,26 +1,66 @@
-import re
+# Copyright 2019 Jacques Supcik
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
 import logging
-from mkdocs.plugins import BasePlugin
+import os
+import re
+
 import mkdocs.structure
+from mkdocs import utils
+from mkdocs.config import config_options
+from mkdocs.plugins import BasePlugin
 
 logger = logging.getLogger("mkdocs.plugin.select-files")
+
 
 class SelectFiles(BasePlugin):
 
     config_scheme = (
-        ('select', mkdocs.config.config_options.Type(mkdocs.utils.string_types, default='(\d+)')),
-        ('where', mkdocs.config.config_options.Type(mkdocs.utils.string_types, default='lambda x: int(x) >= 0')),
+        ('disabled_if_env', config_options.Type(utils.string_types)),
+        ('select', config_options.Type(utils.string_types, default='(\d+)')),
+        ('where', config_options.Type(
+            mutils.string_types, default='lambda x: int(x) >= 0')),
     )
 
+    def __init__(self):
+        self.disabled = False
+
+    def on_config(self, config):
+        if 'disabled_if_env' in self.config:
+            env_name = self.config['disabled_if_env']
+            if env_name:
+                self.disabled = os.environ.get(env_name) == '1'
+                if self.disabled:
+                    logger.warning(
+                        'select-file is disabled (set environment variable %s to 0 to enable)', env_name)
+
     def on_files(self, files, **kwargs):
+        if self.disabled:
+            return files
+
         logger.debug("Filtering files")
-        
+        # some variables that the user can use in the select lambda:
+        now = datetime.datetime.isoformat()
+        sfc = os.getenv('SELECT_FILE_CONDITION')
+
         try:
             select = re.compile(self.config["select"])
         except Exception as e:
             logger.error("Error parsing select regular expression : %s", e)
             return files
-        
+
         try:
             where = eval(self.config["where"])
         except Exception as e:
